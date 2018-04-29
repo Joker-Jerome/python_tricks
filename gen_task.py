85119647a2798acaa035e50264e2984faf0e1715
Jerome Yu <jerome1993@qq.com>
2018-04-29 12:03:47 -0400

    rpy2

--- gen_task.py
+++ gen_task.py
@@ -10,15 +10,19 @@ method_nam = sys.argv[2]
 # datasets
 real_data_list = ["Brain", "HNSC", "TCGA_LUNG", "melanoma", "Metabric"]
 
-# stepsize 
-par1_vec = [0.01, 0.1, 0.5, 1, 2]
+# splitrule
+par1_vec = ["logrank", "logrankscore"]
 
-# maxstep
-par2_vec = [100, 250, 500]
+# ntree
+par2_vec = [100, 200, 500]
+
+# nodedepth
+par3_vec = [3,5,7]
 
 with open(file_name, "w") as fo:
 	for dataset in real_data_list:
 		for par1 in par1_vec:
 			for par2 in par2_vec:
-				fo.write("module load R; Rscript --vanila " + method_nam + "_multi.R /ysm-gpfs/project/lz276/Project3/data_survival/" + dataset + " " + dataset)
-				fo.write(" " + str(par1) + " " + str(par2) + "\n")
+				for par3 in par3_vec:
+					fo.write("module load R; Rscript --vanilla " + method_nam + "_multi.R /ysm-gpfs/project/lz276/Project3/data_survival/" + dataset + " " + dataset)
+					fo.write(" " + str(par1) + " " + str(par2) + " " + str(par3) + "\n")
