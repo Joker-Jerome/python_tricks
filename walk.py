6ba7d1dd4d8ac609a888335d5a7025ea1781b46f
Jerome Yu <jerome1993@qq.com>
2020-01-06 10:40:11 -0500

    walk

---
+++ walk.py
@@ -0,0 +1,11 @@
+import os
+
+print("root prints out directories only from what you specified")
+print("dirs prints out sub-directories from root")
+print("files prints out all files from root and directories")
+print("*" * 20)
+
+for root, dirs, files in os.walk("/var/log"):
+    print(root)
+    print(dirs)
+    print(files)
